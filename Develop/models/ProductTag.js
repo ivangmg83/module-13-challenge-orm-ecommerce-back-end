723524af2b092d3d573c94b0f987c188962bdba6
ivangmg <ivangmg@hotmail.com>
2023-02-01 18:48:28 -0800

    model names corrected in references

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -16,14 +16,14 @@ ProductTag.init(
    product_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'Product',
+        model: 'product',
         key: 'product_id',
       },      
     },
     tag_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'Tag',
+        model: 'tag',
         key: 'tag_id',
       },      
     },
