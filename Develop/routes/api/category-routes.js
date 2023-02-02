6e9e853b031c6a3845efb4f1a3896152f91af21a
ivangmg <ivangmg@hotmail.com>
2023-02-01 20:37:52 -0800

    api routes added

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,23 +6,63 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then((data) => {
+    res.json(data);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id).then((data) => {
+    res.json(data);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then((newCategory) => {
+      res.json(newCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Book.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      category_id: req.body.id,
+      categoty_name: req.body.categoty_name,
+    },
+    {
+      // Gets the category based on the id given in the request parameters
+      where: {
+        category_id: req.body.id,
+      },
+    }
+  )
+    .then((updatedCategory) => {
+      // Sends the updated book as a json response
+      res.json(updatedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      category_id: req.body.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
